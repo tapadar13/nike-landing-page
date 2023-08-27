96b1536ace00e570324b2630373d1231742d9142
tapadar13 <monsurismyname@gmail.com>
2023-08-27 13:48:01 +0530

    Added Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => (
   <main className="relative">
     <Nav />
     <section className="xl: padding-1  wide:padding-r padding-b">
-      {/* <Hero /> */}
+      <Hero />
     </section>
     <section className="padding">{/* <PopularProducts /> */}</section>
     <section className="padding">{/* <SuperQuality /> */}</section>
