98d4193438eba7e7b341f2543c8f041ac2c58064
tapadar13 <monsurismyname@gmail.com>
2023-08-28 08:20:24 +0530

    Implemented Popular Products section

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ const App = () => (
     <section className="xl: padding-1  wide:padding-r padding-b">
       <Hero />
     </section>
-    <section className="padding">{/* <PopularProducts /> */}</section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
     <section className="padding">{/* <SuperQuality /> */}</section>
     <section className="padding-x py-10">{/* <Services /> */}</section>
     <section className="padding">{/* <SpecialOffer /> */}</section>
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,29 @@
+import { products } from "../constants/index";
+import PopularProductCard from "../components/PopularProductCard";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h1 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h1>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design and value Embark on a
+          {/* Journey of Unparalleled Quality and Style. Explore our Coveted
+          Collection, where Comfort, Design, and Value unite to redefine your
+          footwear experience */}
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
