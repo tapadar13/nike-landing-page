7ab7be696f40b5b5d2a9782941db05aaffb3b73d
tapadar13 <monsurismyname@gmail.com>
2023-09-04 21:51:59 +0530

    implemented Special Offer section

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,11 @@ const App = () => (
     <section className="padding-x py-10">
       <Services />
     </section>
-    <section className="padding">{/* <SpecialOffer /> */}</section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
     <section className="bg-pale-blue padding">
-      {/* <CustomerReviews /> */}
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
       {/* <Subscribe /> */}
